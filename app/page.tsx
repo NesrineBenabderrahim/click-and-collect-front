280759ffaeffc452f4c43ae8c950fbdcf10c484d
nesrine benabderrahim <nesrine.abd.softavera@gmail.com>
2025-06-20 10:40:47 +0100

    feat: add css template app

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,37 @@
-
-import Image from "next/image";
+"use client";
+import { Fade } from "react-awesome-reveal";
 import Container from "./components/Container";
-import HomePhoto from "./components/HomePhoto";
-import { card, products } from "./utils/products";
 import ProductCart from "./components/product/ProductCart";
-import LocationSearchForm from "./components/form/SearchForm";
-import SearchInput from "./components/form/SearchInput";
-
+import Map from "./components/Map";
+import Banner from "./components/Banner";
+import Gallery from "./components/Gallery";
+import MenuCart from "./cart/MenuCart";
+import { Chatbox } from "./components/Chat Bot/base";
 
 export default function Home() {
-
-  
   return (
     <div className="">
-    <HomePhoto />
-    <Container>
-      <div className="grid grid-cols-1 p-4 item-center   gap-4 lg:gap-12 pt-2 item-center justify-between">
-        <h1 className="text-[20px] ">Nos magasins à proximité</h1>
-          <SearchInput />
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-          {/* {Object.values(card.shoplist).map((item)=><div  key={item.Company}> <ProductCart  data={item} id={Object.keys(item)}/></div> )} */}
-
-          {Object.values(card.shoplist).map((item:any)=><div  key={item.Company}> <ProductCart  data={item} /></div> )}
-        
-          
-        S</div>
-      </div>
-      
-    </Container>
+      <Banner />
+      <MenuCart />
+      <Chatbox />
+      <Map />
+      <Container>
+        <div className="grid grid-cols-1 item-center lg:gap-12 item-center justify-between">
+          <div>
+            <div className="mx-auto max-w-7xl py-2 px-6 pt-[6%]" id="about-section">
+              <div className="px-4 mb-4">
+                <Fade direction={"up"} delay={400} cascade damping={1e-1} triggerOnce={true}>
+                  <h3 className="text-3xl lg:text-5xl font-semibold text-lightgrey">
+                    Nos restaurants à proximité
+                  </h3>
+                </Fade>
+              </div>
+              <ProductCart />
+            </div>
+          </div>
+        </div>
+        <Gallery />
+      </Container>
     </div>
-    
   );
 }
