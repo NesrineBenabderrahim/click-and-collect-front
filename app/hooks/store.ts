f80f11d44cd306779ba802c8d259a5a741563d87
nesrine benabderrahim <nesrine.abd.softavera@gmail.com>
2025-06-20 10:53:43 +0100

    refactor: add `store hooks`

---
+++ app/hooks/store.ts
@@ -0,0 +1,17 @@
+"use client";
+
+import { proxy } from "valtio";
+
+type StoreType = {
+  isValidation: boolean;
+};
+
+export const store = proxy<StoreType>({
+  isValidation: false,
+});
+
+export function setIsValidation(isValidation: boolean): void {
+  store.isValidation = isValidation;
+}
+
+export default store;
