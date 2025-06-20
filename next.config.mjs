5d2a978007d4bbdcfb1c75fc8994b6998a2efb2c
nesrine benabderrahim <nesrine.abd.softavera@gmail.com>
2025-06-20 10:40:08 +0100

    build: build next and react packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['www.commande-pizzatime.fr','recette-catalogue.aureskonnect.com'],
+        domains: ['www.commande-pizzatime.fr','recette-catalogue.aureskonnect.com','pizzatime.fr'],
     }
 };
 
 export default nextConfig;
+
